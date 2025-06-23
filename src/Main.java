0beee9f22fa985d01f857bca45ed57a01abda025
nagira syunya <241004@std.hi-joho.ac.jp>
2025-06-23 11:40:42 +0900

    プライベートからpublicに変更、及び教科書コードの一部分抜けてたのを追記しました

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
 public class Main {
         public static void main(String[] args) {
-            Hero h = new Hero();
+            // 1.勇者を生成
+            Hero h = new Hero("ミナト");
+            // 2.フィールドに初期値をセット
             h.name = "ミナト";
             h.hp = 100;
+            System.out.println("勇者" + h.name + "を生み出しました！");
+            // 3.勇者のメソッドを呼び出してゆく
+            h.sit(5);
+            h.slip();
+            h.sit(25);
+            h.run();
+            int yusha_hp = 100;
+            int Matango1_hp = 50;
+            int Matango2_hp = 48;
+            String yusha_name = "ミナト";
+            int matango1_level = 10;
+            int matango2_level = 10;
+            System.out.println(yusha_name + "は5秒座った！");
+            yusha_hp += 5;
+            System.out.println("HPが5ポイントは回復した！");
 
             Matango m1 = new Matango();
             m1.hp = 50;
@@ -15,6 +32,6 @@ public class Main {
             h.slip();
             m1.run();
             m2.run();
-            h.run();
+            h.run();;
         }
 }
